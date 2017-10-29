84b26d1fcc54949f80c827b635f63c9a069994cf
Aliaksei Urbanski <mim@mim.pw>
2017-10-29 21:11:22 +0300

    Add a CRUD API for User model

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from .permissions import IsNotCurrentUser
+from .serializers import UserSerializer
+
+
+user_model = get_user_model()
+
+
+class UserViewMixin(object):
+    serializer_class = UserSerializer
+    queryset = (
+        user_model.objects.exclude(is_superuser=True).exclude(is_staff=True)
+    )
+
+
+class UserList(UserViewMixin, generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class UserView(UserViewMixin, generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated, IsNotCurrentUser)
