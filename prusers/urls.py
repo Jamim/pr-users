6632bbe757eb548748594ff414b5255d394dd81b
Aliaksei Urbanski <mim@mim.pw>
2017-11-13 23:24:45 +0300

    Move endpoints to /api/

--- prusers/urls.py
+++ prusers/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^api-auth/',
+    url(r'^api/auth/',
         include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^users/', include('users.urls', namespace='users')),
+    url(r'^api/users/', include('users.urls', namespace='users')),
 ]
