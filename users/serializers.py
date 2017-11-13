c0f6937e5a1936cc101620a4d7e5c5c2177a4c79
Aliaksei Urbanski <mim@mim.pw>
2017-11-13 23:23:23 +0300

    Add password field to UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -9,4 +9,14 @@ user_model = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = user_model
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = (
+            'id', 'username', 'first_name', 'last_name', 'email', 'password'
+        )
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = user_model(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
